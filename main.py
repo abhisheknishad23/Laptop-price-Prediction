adeae12e1e667091f583be421c89cc5a930e0139
abhisheknishad23 <iabhisheku4@gmail.com>
2026-06-11 22:10:09 +0530

    laptopprice

---
+++ main.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pickle
+import numpy as np
+import pandas as pd
+# load file
+pipe = pickle.load(open('pipe.pkl', 'rb'))
+df = pickle.load(open('df.pkl', 'rb'))
+
+st.title("Laptop Price Predictor")
+
+# frontend inputs
+company = st.selectbox('Brand', df['Company'].unique())
+type_name = st.selectbox('Type', df['TypeName'].unique())
+ram = st.selectbox('RAM(in GB)', [2, 4, 6, 8, 12, 16, 24, 32, 64])
+weight = st.number_input('Weight of the Laptop')
+touchscreen = st.selectbox('Touchscreen', ['No', 'Yes'])
+ips = st.selectbox('IPS Display', ['No', 'Yes'])
+screen_size = st.number_input('Screen Size (Inches)')
+resolution = st.selectbox('Screen Resolution', [
+    '1920x1080', '1366x768', '1600x900', '3840x2160', '3200x1800',
+    '2880x1800', '2560x1600', '2560x1440', '2304x1440'
+])
+cpu = st.selectbox('CPU Brand', df['Cpu brand'].unique())
+hdd = st.selectbox('HDD(in GB)', [0, 128, 256, 512, 1000, 2000])
+ssd = st.selectbox('SSD(in GB)', [0, 8, 128, 256, 512, 1024])
+gpu = st.selectbox('GPU Brand', df['Gpu brand'].unique())
+os = st.selectbox('OS', df['os'].unique())
+
+
+if st.button('Predict Price'):
+    touchscreen = 1 if touchscreen == 'Yes' else 0
+    ips = 1 if ips == 'Yes' else 0
+
+    X_res = int(resolution.split('x')[0])
+    Y_res = int(resolution.split('x')[1])
+    ppi = ((X_res ** 2) + (Y_res ** 2)) ** 0.5 / screen_size
+
+    query_data = {
+        'Company': [company],
+        'TypeName': [type_name],
+        'Ram': [ram],
+        'Weight': [weight],
+        'Touchscreen': [touchscreen],
+        'Ips': [ips],
+        'ppi': [ppi],
+        'Cpu brand': [cpu],
+        'HDD': [hdd],
+        'SSD': [ssd],
+        'Gpu brand': [gpu],
+        'os': [os]
+    }
+
+
+    query_df = pd.DataFrame(query_data)
+
+    log_prediction = pipe.predict(query_df)[0]
+    actual_price = int(np.exp(log_prediction))
+
+    st.title(f"The predicted price of this configuration is: ₹{actual_price:,}")
